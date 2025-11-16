41a19b47e0e011a724656a6da2fa2454e7abc62f
Farhana Ahmed <farhanameem18@gmail.com>
2025-11-16 23:37:36 +0600

    Initial commit

---
+++ src/main/java/com/example/oop_project_group02_bata_shoe_company/Farhana_2312079/productionSupervisor/DefectController.java
@@ -0,0 +1,7 @@
+package com.example.oop_project_group02_bata_shoe_company.Farhana_2312079.productionSupervisor;
+
+public class DefectController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
