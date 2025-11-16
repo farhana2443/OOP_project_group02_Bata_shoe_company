cea58c5edc8d8eacc48d7d38951d950bd9ccee3b
Farhana Ahmed <farhanameem18@gmail.com>
2025-11-16 18:51:41 +0600

    Initial commit

---
+++ src/main/java/com/example/oop_project_group02_bata_shoe_company/FinanceDashboard.java
@@ -0,0 +1,32 @@
+package com.example.oop_project_group02_bata_shoe_company;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class FinanceDashboard
+{
+    @javafx.fxml.FXML
+    private Button btnPayrollProcessing;
+    @javafx.fxml.FXML
+    private Button btnVendorPayments;
+    @javafx.fxml.FXML
+    private Button btnDailyTransactions;
+    @javafx.fxml.FXML
+    private Button btnMonthlyReport;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void openVendorPayments(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void openMonthlyReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void openPayroll(ActionEvent actionEvent) {
+    }
+}
