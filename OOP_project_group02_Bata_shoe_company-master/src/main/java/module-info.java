cc9f0369b62a81b69cc17419487ec9f179acb13d
Saba24000 <2221648@iub.edu.bd>
2025-12-05 11:53:05 +0600

    Added my FXML and controllers

---
+++ OOP_project_group02_Bata_shoe_company-master/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.oop_project_group02_bata_shoe_company {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.oop_project_group02_bata_shoe_company to javafx.fxml;
+    exports com.example.oop_project_group02_bata_shoe_company;
+    exports com.example.oop_project_group02_bata_shoe_company.Summy;
+    opens com.example.oop_project_group02_bata_shoe_company.Summy to javafx.fxml;
+}
